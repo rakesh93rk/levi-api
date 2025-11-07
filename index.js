c88d5a686c4c3f14188a73251a7b572405a89f85
rakesh93rk <shivaprasad93sh@gmail.com>
2025-11-07 09:02:13 +0530

    Fix import path for staffRoutes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const staffRoutes = require("./routes/staffRoutes");
+const staffRoutes = require("./staffRoutes");
 
 const app = express();
 app.use(cors());
